60b805e983d4319b96061bd07dec6edf563326fd
AnasSabbag <86312422+AnasSabbag@users.noreply.github.com>
2025-02-26 13:26:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //your JS code here. If required.
+function handleSubmit() {
+	const name= document.getElementById("name").value;
+	const age= document.getElementById("age").value;
+	if(name==="" || age===undefined){
+		alert("Please enter valid details.")
+		return;
+	}
+
+	let myPromise = new Promise(function(myResolve, myReject) {
+  
+
+// The producing code (this may take some time)
+
+  if (age >= 18) {
+    myResolve("OK");
+  } else {
+    myReject("Error");
+  }
+});
+
+myPromise.then(
+  function(value) {alert("Welcome, . You can vote.");},
+  function(error) {alert("Oh sorry . You aren't old enough.");}
+);
+	
+}
